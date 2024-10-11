86a8a677721cb7ba79d91d6e29bb1e69001c9e61
Albalad <alvaro.albalad@gmail.com>
2024-10-11 18:40:40 +0000

    ajustes del index y del css

--- src/app.js
+++ src/app.js
@@ -5,7 +5,11 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const valorCarta = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const paloCarta = ["♦", "♥", "♠", "♣"];
+const randomizador = arr => arr[Math.floor(Math.random() * arr.length)];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const carta = document.querySelector(".card");
 };
